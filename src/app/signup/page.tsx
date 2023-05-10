9792c0008611a7ab3b86cc593e0ccc172cefc314
Felix Mokross <felix.mokross@zuehlke.com>
2023-05-10 17:33:46 +0200

    Add server action for signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,17 +10,29 @@ import {
 import Link from "next/link";
 import { pick } from "accept-language-parser";
 import { headers } from "next/headers";
+import { safeRedirect } from "@/utils";
+import { getSession } from "@/session.server";
+import { createUser } from "@/models/users.server";
+import { redirect } from "next/navigation";
 
-export default function Signup() {
-  // const actionData = useActionData<typeof action>();
+export default function Signup({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const suggestedLocale = getSuggestedLocale();
   const suggestedCurrency =
     (suggestedLocale && getSuggestedCurrencyForLocale(suggestedLocale)) ||
     "USD";
+  const redirectTo = searchParams["redirectTo"];
 
   return (
     <div className=" w-full px-4 py-10">
-      <form method="post" noValidate className="mx-auto flex max-w-sm flex-col">
+      <form
+        action={submit}
+        noValidate
+        className="mx-auto flex max-w-sm flex-col"
+      >
         <Link href="/" aria-label="Home">
           <LogoSmall className="h-10 w-auto" />
         </Link>
@@ -43,6 +55,7 @@ export default function Signup() {
             defaultValue={suggestedCurrency}
             // error={actionData?.errors?.refCurrency}
           />
+          <input type="hidden" value={redirectTo} />
         </div>
         <Button type="submit" variant="primary" className="mt-10">
           Start Using Cashfolio
@@ -58,4 +71,32 @@ function getSuggestedLocale() {
 
   // TODO there seems to be an issue here: for a request with en-US in the accepted languages, en-Dsrt-US is picked, although en-US is also supported
   return pick(getLocales(), acceptLanguageHeader) || undefined;
+}
+
+async function submit(formData: FormData) {
+  "use server";
+
+  const preferredLocale = formData.get("preferredLocale");
+  const refCurrency = formData.get("refCurrency");
+  const redirectTo = safeRedirect(formData.get("redirectTo"));
+
+  if (typeof preferredLocale !== "string" || preferredLocale.length === 0) {
+    // TODO how can we return an error to the client?
+    throw new Error("Preferred locale is required");
+  }
+
+  if (typeof refCurrency !== "string" || refCurrency.length === 0) {
+    return new Error("Reference currency is required");
+  }
+
+  const session = await getSession();
+  const userId = session.get("userId");
+
+  await createUser({
+    auth0UserId: userId,
+    refCurrency,
+    preferredLocale,
+  });
+
+  redirect(redirectTo);
 }
